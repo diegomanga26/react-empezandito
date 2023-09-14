d33fe9ea980c5f6337ba10266ac3f7626ea97d0c
Lucheto04 <lucho.lgrg04@gmail.com>
2023-09-14 16:36:22 -0500

    feat: :tada: .
    .

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import Registrico from './registro.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Registrico/>
+  </React.StrictMode>,
+)
---
+++ src/registro.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function Registrico() {
+    const registrar = async (e) => {
+        e.preventDefault();
+        let correo = document.querySelector("#sancocho").value;
+        let contraseña = document.querySelector("#arroz").value;
+        let registrarse = { correo, contraseña };
+        console.log(registrarse);
+
+        const request = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(registrarse)
+        };
+        let respuesta = await fetch("http://deboponerlaurlaqui", request);
+        const respuestaJson = await respuesta.json();
+        console.log(respuestaJson);
+    }
+    return (
+        <div className='row'>
+            <div className='card container mt-3 col-sm-10 col-lg-4'>
+                <div className='card-body'>
+                    <form onSubmit={registrar}>
+                        <div className="mb-3">
+                            <label className="form-label">Ingrese su correo:</label>
+                            <input type="email" className="form-control" id='sancocho'/>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Ingrese su contraseña:</label>
+                            <input type="password" className="form-control" id='arroz'/>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Registrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
