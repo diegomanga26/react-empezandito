13122cdc247a0f965d1ba613947c5ce555e20962
diegomanga26 <diegoalexandermanga@gmail.com>
2023-09-26 16:24:11 -0500

    feat: :heavy_plus_sign: Añadiendo protocolo CORS.
    Se añade protocolo CORS para las peticiones del front al back.

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import routerEnd from "./routes/index.js";
+import cors from 'cors';
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    optionsSuccessStatus: 204
+};
 
 dotenv.config();
 const app = express();
 
 // Inicio del servidor
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use('/', routerEnd)
+app.use('/', routerEnd);
 
-app.listen( () => {
-    console.log(`http://${process.env.VITE_HOST_BACK}:${process.env.VITE_PORT_BACK}`);
+const host = process.env.VITE_HOST_BACK;
+const port = process.env.VITE_PORT_BACK;
+
+app.listen( port, host,  () => {
+    console.log(`http://${host}:${port}`);
 });
