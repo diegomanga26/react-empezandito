4d016f8a283c93109d201f585daea0dc7e7bc784
diegomanga26 <diegoalexandermanga@gmail.com>
2023-09-26 16:25:51 -0500

    refactor: :truck: Refactorizando la conexión "atlas.js".
    Se actualiza el método de conexión a la base de datos por aviso de obsolencia.

--- backend/db/config/atlas.js
+++ backend/db/config/atlas.js
@@ -1,14 +1,10 @@
 import {MongoClient} from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
-export async function con(){
+export async function con() {
     try {
         const uri = `mongodb+srv://${process.env.atlasUser}:${process.env.atlasPassword}@cluster0.2f7rqil.mongodb.net/${process.env.atlasDb}`;
-        const options = {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        };
-        const client = await MongoClient.connect(uri, options);
+        const client = await MongoClient.connect(uri);
         return client.db();
     } catch (error) {
         return {status: 500, message: error};
